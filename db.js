198065cd346291c26b734cf8b1e7790f97197516
liana5555 <baloghildiko55@gmail.com>
2024-10-04 21:14:30 +0200

    Fix: refactored auth.js in controllers

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2')
+const mysql = require("mysql2/promise");
+const bluebird = require("bluebird");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
-module.exports = db
+module.exports = db;
